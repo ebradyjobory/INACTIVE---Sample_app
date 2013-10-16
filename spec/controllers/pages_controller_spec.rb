6ac9efc58ee5275806a6f86a3d265952ec10ca60
Essam Al Joubori <essam.aljoubori@gmail.com>
2013-10-16 11:32:16 -0700

    Create static pages with embedded ruby and successful tests.

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -12,8 +12,12 @@ render_views
     it "should have the right title" do
     get 'home'
     response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Home")
+    end
 
-  end
+    it "should have a non-blank body" do
+      get 'home'
+      response.body.should_not =~ /<body>\s*<\/body>/
+    end
 end
 
   describe "GET 'contact'" do
